455bb9b1b34e4645696045178d043afaca9849d8
AMAN KUMAR CHHARI <140314215+chhari07@users.noreply.github.com>
2024-09-17 22:11:16 +0530

    Update CardGrid.jsx

--- src/CardGrid.jsx
+++ src/CardGrid.jsx
@@ -1,68 +1,70 @@
-import React from 'react'
+import React from 'react';
+import './CardGrid.css';
 
 const CardGrid = () => {
   return (
-    <div>
-      <section className="text-gray-700 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4 text-center">
-           
-            <div className="p-4 w-full">
-              <div className="relative h-72 max-w-full mx-auto px-5 py-8 group rounded-xl bg-gray-200 overflow-hidden shadow-xl">
-                <img src="https://inda.rcb.ac.in/sewagedata/assets/img/mohfw.png" alt="" className="absolute w-full h-full inset-0 object-cover" />
-                <div className="absolute inset-0 w-full h-full rounded-xl bg-black bg-opacity-0 transition duration-500 backdrop-filter group-hover:bg-opacity-20 group-hover:backdrop-blur"></div>
-                <div className="absolute inset-x-5 text-white">
-                  <h2 className="text-4xl font-semibold mb-2">HEALTH MINISTRY</h2>
-                  <p className="hidden group-hover:block text-black">Whether you're looking to save money spending abroad or sticking to your budget, get more from your money with our Standard account.</p>
-                </div>
-                
-              </div>
-            </div>
-
-         
-            <div className="p-4 w-full">
-              <div className="relative h-72 max-w-full mx-auto px-5 py-8 group rounded-xl bg-gray-200 overflow-hidden shadow-xl">
-                <img src="https://uxdt.nic.in/wp-content/uploads/2021/12/fit-india-fit-india-01-01.jpg?x69772" alt="" className="absolute w-full h-full inset-0 object-cover" />
-                <div className="absolute inset-0 w-full h-full rounded-xl bg-black bg-opacity-0 transition duration-500 backdrop-filter group-hover:bg-opacity-20 group-hover:backdrop-blur"></div>
-                <div className="absolute inset-x-5 text-white">
-                  <h2 className="text-4xl font-semibold mb-2">FIT INDIA</h2>
-                  <p className="hidden group-hover:block text-black">Whether you're looking to save money spending abroad or sticking to your budget, get more from your money with our Standard account.</p>
-                </div>
-               
-              </div>
-            </div>
-
-           
-            <div className="p-4 w-full">
-              <div className="relative h-72 max-w-full mx-auto px-5 py-8 group rounded-xl bg-gray-200 overflow-hidden shadow-xl">
-                <img src="https://assets.revolut.com/media/plans-tiles/Cards-Tiles-Standard.png" alt="" className="absolute w-full h-full inset-0 object-cover" />
-                <div className="absolute inset-0 w-full h-full rounded-xl bg-black bg-opacity-0 transition duration-500 backdrop-filter group-hover:bg-opacity-20 group-hover:backdrop-blur"></div>
-                <div className="absolute inset-x-5 text-white">
-                  <h2 className="text-4xl font-semibold mb-2">STANDARD</h2>
-                  <p className="hidden group-hover:block">Whether you're looking to save money spending abroad or sticking to your budget, get more from your money with our Standard account.</p>
-                </div>
-              
-              </div>
-            </div>
-
-          
-            <div className="p-4 w-full">
-              <div className="relative h-72 max-w-full mx-auto px-5 py-8 group rounded-xl bg-gray-200 overflow-hidden shadow-xl">
-                <img src="https://assets.revolut.com/media/plans-tiles/Cards-Tiles-Standard.png" alt="" className="absolute w-full h-full inset-0 object-cover" />
-                <div className="absolute inset-0 w-full h-full rounded-xl bg-black bg-opacity-0 transition duration-500 backdrop-filter group-hover:bg-opacity-20 group-hover:backdrop-blur"></div>
-                <div className="absolute inset-x-5 text-white">
-                  <h2 className="text-4xl font-semibold mb-2">STANDARD</h2>
-                  <p className="hidden group-hover:block">Whether you're looking to save money spending abroad or sticking to your budget, get more from your money with our Standard account.</p>
-                </div>
-                
-              </div>
-           
-            </div>
-          </div>
+    <div className="card-container">
+      {/* Effect-1 */}
+      <div className="crd crd--effect-1">
+        <div
+          className="crd-img"
+          style={{
+            backgroundImage:
+              'url("https://images.unsplash.com/photo-1541881702266-f2dd28ed4415?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=340")',
+          }}
+        ></div>
+        <div className="crd-info">
+          <h2 className="crd-heading">Lumière</h2>
+          <p className="crd-text">And then that begins...</p>
         </div>
-      </section>
-    </div>
-  )
-}
+      </div>
 
-export default CardGrid
+      {/* Effect-2 */}
+      <div className="crd crd--effect-2">
+        <div
+          className="crd-img"
+          style={{
+            backgroundImage:
+              'url("https://images.unsplash.com/photo-1447405866403-2dc651878fd6?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=340")',
+          }}
+        ></div>
+        <div className="crd-info">
+          <h2 className="crd-heading">Wolken</h2>
+          <p className="crd-text">And then that begins...</p>
+        </div>
+      </div>
+
+      {/* Effect-3 */}
+      <div className="crd crd--effect-3">
+        <div
+          className="crd-img"
+          style={{
+            backgroundImage:
+              'url("https://images.unsplash.com/photo-1503517296638-b2b666927c0d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=340")',
+          }}
+        ></div>
+        <div className="crd-info">
+          <h2 className="crd-heading"></h2>
+          <p className="crd-text">    </p>
+        </div>
+      </div>
+
+      {/* Effect-4 */}
+      <div className="crd crd--effect-4">
+        <div
+          className="crd-img"
+          style={{
+            backgroundImage:
+              'url("https://images.unsplash.com/photo-1523441888894-57962297f940?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=800")',
+          }}
+        ></div>
+        <div className="crd-info">
+          <h2 className="crd-heading">Keine Worte.</h2>
+          <p className="crd-text">And then that begins...</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardGrid;
